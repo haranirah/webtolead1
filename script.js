0cce8cd263d944f48b17f9293672a1bd1d0b8ebf
Hariharan <hariharan.u192420@gmail.com>
2024-11-21 15:16:05 +0530

    fgjghjgh

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ function beforesumbit(event)
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");  // string --> date( en_IN )
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formattedDate;   
-    }
-    else
-    {
-        alert("Please check the recApthchA");
-        event.preventDefault();
+    outputdate.value = formattedDate;
+    }else{
+            alert("Please check the recApthchA");
+            event.preventDefault();
     }
 }
 function timestamp()
